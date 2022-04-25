eb0e19cc0ccf3cca9cbab81b3c59eaaa387fff89
saqeeb khan <saqeebkhan8271@gmail.com>
2022-04-26 00:58:09 +0530

    2nd commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,7 +1,22 @@
 package com.example.demo.model;
 
-import javax.persistence.Entity;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.Set;
 
+@Data
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String State;
+    private String city;
+    private String address;
+    private String image;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Set<Role> roles;
 }
--- src/main/java/com/example/demo/model/Worker.java
+++ src/main/java/com/example/demo/model/Worker.java
@@ -1,7 +1,21 @@
 package com.example.demo.model;
 
-import javax.persistence.Entity;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
 @Entity
 public class Worker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private String state;
+    private String city;
 }
